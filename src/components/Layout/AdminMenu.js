1a69d0d400d7d54361d31e2720e2087ddcc53475
unknown <Deepak Singh>
2023-06-22 23:03:30 +0530

    new feature added

--- src/components/Layout/AdminMenu.js
+++ src/components/Layout/AdminMenu.js
@@ -7,9 +7,26 @@ function AdminMenu() {
             <div className="text-center">
                 <div className="list-group">
                     <h4>Admin Pannel</h4>
-                    <NavLink to="/dashboard/admin/create-category" className="list-group-item list-group-item-action">Create Category</NavLink>
-                    <NavLink to="/dashboard/admin/create-product" className="list-group-item list-group-item-action">Creat Product</NavLink>
-                    <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action">Users</NavLink>
+                    <NavLink
+                        to="/dashboard/admin/create-category"
+                        className="list-group-item list-group-item-action">
+                        Create Category
+                    </NavLink>
+                    <NavLink
+                        to="/dashboard/admin/create-product"
+                        className="list-group-item list-group-item-action">
+                        Creat Product
+                    </NavLink>
+                    <NavLink
+                        to="/dashboard/admin/products"
+                        className="list-group-item list-group-item-action">
+                        Products
+                    </NavLink>
+                    <NavLink
+                        to="/dashboard/admin/users"
+                        className="list-group-item list-group-item-action">
+                        Users
+                    </NavLink>
                 </div>
             </div>
         </>
